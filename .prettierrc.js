85ac1359cc4439dbc66d7229fad22b8a49f0fa1a
cha <chyb627@naver.com>
2022-10-03 22:47:31 +0900

    tailwind, babel, eslint, prettier, .vscode

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // https://prettier.io/docs/en/configuration.html 참고
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  semi: true,
+  printWidth: 130,
+  singleQuote: true,
+  trailingComma: 'all',
+};
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,48 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  let resProduction = {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      'babel-plugin-styled-components',
+      'tailwind-rn',
+      'transform-remove-console',
+      [
+        'module-resolver',
+        {
+          root: './src',
+          alias: {
+            '~': './src',
+            '~pages': './src/pages',
+            '~store': './src/store',
+            '~components': './src/components',
+            '~slice': './src/slice',
+            '~assets': './src/assets',
+            '~hooks': './src/hooks',
+            '~lib': './src/lib',
+            '~shared': './src/components/shared',
+            '~ui': './src/components/ui',
+          },
+        },
+      ],
+      [
+        '@babel/plugin-proposal-decorators',
+        {
+          legacy: true,
+        },
+      ],
+      // [
+      //   '@babel/plugin-proposal-class-properties',
+      //   {
+      //     loose: true,
+      //   },
+      // ],
+    ],
+  };
+
+  if (api.env('production')) {
+    return resProduction;
+  }
+
+  resProduction.plugins.splice(resProduction.plugins.indexOf('transform-remove-console'), 1);
+
+  return resProduction;
 };
