2d485e9bfd1ac0594bd44c672f5d9387e452603e
cha <chyb627@naver.com>
2022-10-03 22:58:37 +0900

    react-native-vector-icons

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function App() {
   return (
     <SafeAreaView>
       <Text className="underline text-3xl">Hello, RN!</Text>
+      <Icon name="home" size={42} />
     </SafeAreaView>
   );
 }
